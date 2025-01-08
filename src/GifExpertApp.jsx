843eb256dfd959f8eea2f76f71ee531801241d9c
Axel Olea <axeloleabarrera@gmail.com>
2025-01-07 23:32:18 -0300

    First Commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    const onAddCategory = (newCategory) => {
+
+        if ( categories.includes(newCategory)) return;
+
+        setCategories([newCategory, ...categories ]);
+        // setCategories(cat => [...cat, 'Valorant']);
+    };
+
+  return (
+    <>
+        {/* Título */}
+        <h1>GifExpertApp</h1>
+
+        {/* Input */}
+       <AddCategory 
+            //setCategories={setCategories}
+            onNewCategory={value => onAddCategory(value)}
+            currentCategories={ categories }
+       />
+
+        {/* Listado de Gifs */}
+    
+        {
+        categories.map(category => {
+            return (
+                <GifGrid 
+                    key={category}  
+                    category={category} 
+                />
+            )
+        })
+        }
+        
+
+        {/* Gif Item */}
+
+    </>
+  )
+}
